2b3154187d286c90ccf11d3c15e3bb850cb8ac62
YueHonghui <hongf.yue@hotmail.com>
2016-09-13 19:02:20 +0800

    backup

--- httpparam.go
+++ httpparam.go
@@ -1,19 +1,41 @@
-package httpparam
+package gohttpparam
 
 import (
-	"errors"
 	"net/http"
 	"reflect"
+	"strconv"
 	"strings"
+	"sync"
 
-	"github.com/rs/xmux"
+	"github.com/YueHonghui/gotools"
+	"github.com/rs/xhandler"
+	//	"github.com/rs/xmux"
+	"golang.org/x/net/context"
 )
 
-type HttpparamError error
+type ErrTagInvalid struct {
+	Field string
+}
 
-var (
-	TagInvalid HttpparamError = errors.New("TagInvalid")
-)
+func (e *ErrTagInvalid) Error() string {
+	return "tag of field " + e.Field + " invalid"
+}
+
+type ErrValueInvalid struct {
+	Param string
+}
+
+func (e *ErrValueInvalid) Error() string {
+	return "param " + e.Param + " invalid"
+}
+
+type ErrParamRequired struct {
+	Param string
+}
+
+func (e *ErrParamRequired) Error() string {
+	return "param " + e.Param + " required"
+}
 
 type ParamType int
 
@@ -28,28 +50,31 @@ type tagInfo struct {
 	Required bool
 }
 
-func decodeTag(tag string, t *tagInfo) (bool, error) {
+func decodeTag(field, tag string, t *tagInfo) (bool, error) {
+	errtag := &ErrTagInvalid{
+		Field: field,
+	}
 	tags := strings.Split(tag, " ")
 	t.Required = false
 	for _, tf := range tags {
-		fs := strings.Split(t, "=")
+		fs := strings.Split(tf, "=")
 		if fs[0] == "query" {
 			if len(fs) != 2 {
-				return false, TagInvalid
+				return false, errtag
 			}
 			t.Type = Query
 			t.Name = fs[1]
 			return true, nil
 		} else if fs[0] == "path" {
 			if len(fs) != 2 {
-				return false, TagInvalid
+				return false, errtag
 			}
 			t.Type = Path
 			t.Name = fs[1]
 			return true, nil
 		} else if fs[0] == "required" {
 			if len(fs) != 1 {
-				return false, TagInvalid
+				return false, errtag
 			}
 			t.Required = true
 			return true, nil
@@ -58,47 +83,69 @@ func decodeTag(tag string, t *tagInfo) (bool, error) {
 	return false, nil
 }
 
+func DecodeParams(params interface{}, pathget func(key string) (string, bool), queryget func(key string) (string, bool)) error {
+	values := reflect.ValueOf(params)
+	if values.Kind() != reflect.Ptr {
+		panic("param must be ptr")
+	}
+	values = values.Elem()
+	fields := gotools.DeepFields(values.Type())
+	var taginfo tagInfo
+	for _, t := range fields {
+		if t.Anonymous {
+			continue
+		}
+		fv := values.FieldByName(t.Name)
+		if !fv.IsValid() {
+			continue
+		}
+		var pv interface{}
+		tagstr, ok := t.Tag.Lookup("param")
+		if !ok {
+			continue
+		}
+		ok, err := decodeTag(t.Name, tagstr, &taginfo)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			continue
+		}
+		var pvs string
+		if taginfo.Type == Query {
+			pvs, ok = queryget(taginfo.Name)
+		} else {
+			pvs, ok = pathget(taginfo.Name)
+		}
+		if !ok {
+			if taginfo.Required {
+				return &ErrParamRequired{t.Name}
+			}
+			continue
+		}
+		vetype := t.Type
+		if vetype.Kind() == reflect.Ptr {
+			vetype = vetype.Elem()
+		}
+		switch vetype.Kind() {
+		case reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8, reflect.Int:
+			pv, err := strconv.ParseInt(pvs, 10, 64)
+			if err != nil {
+				return &ErrValueInvalid{t.Name}
+			}
+			if pv < -(1<<(uint64(vetype.Bits())-1)) || pv > (1<<(uint64(vetype.Bits())-1))-1 {
+				return &ErrValueInvalid{t.Name}
+			}
+			if t.Type.Kind() == reflect.Ptr {
+
+			}
+		}
+	}
+}
+
 func NewParamHttpC(f func(ctx context.Context, w http.ResponseWriter, r *http.Request), parampool *sync.Pool) xhandler.HandlerFuncC {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		params := parampool.Get()
-		values := reflect.ValueOf(params)
-		if values.Kind() != reflect.Ptr {
-			panic("param must be ptr")
-		}
-		values = values.Elem()
-		fields := gotools.DeepFields(values.Type())
-		var taginfo tagInfo
-		for _, v := range fields {
-			var pv interface{}
-			tagstr, ok := v.Tag.Lookup("param")
-			if !ok {
-				continue
-			}
-			ok = decodeTag(tagstr, &taginfo)
-			if !ok {
-				continue
-			}
-			var pvs string
-			if taginfo.Type == Query {
-				qvs, ok := r.URL.Query()[taginfo.Name]
-				if !ok {
-					if taginfo.Required {
-						w.WriteHeader(http.StatusBadRequest)
-						w.Write([]byte{"QueryString param "})
-						w.Write([]byte{taginfo.Name})
-						w.Write([]byte{" needed"})
-						return
-					}
-					continue
-				}
-				pvs = qvs[0]
-			} else {
-				pvs = xmux.Param(ctx, taginfo.Name)
-			}
-			switch v.Type.Kind() {
-			case reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8, reflect.Int:
-				pv := strings
-			}
-		}
+
 	}
 }
